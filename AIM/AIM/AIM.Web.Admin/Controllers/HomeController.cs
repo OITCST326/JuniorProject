df124e9c2cbf0414076fd3b6082f3cec2f7d25a4
jordan-ringo <jordan.ringo@oit.edu>
2014-03-17 21:26:15 -0700

    login contact and about changes

--- AIM/AIM/AIM.Web.Admin/Controllers/HomeController.cs
+++ AIM/AIM/AIM.Web.Admin/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace AIM.Web.Admin.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "A.I.M. Admin...";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "5 Programmers of Tomorrow...";
 
             return View();
         }
