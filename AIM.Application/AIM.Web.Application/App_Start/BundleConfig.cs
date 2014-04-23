c174dc83449e9c7ca58775d5ec2a417af2c83030
Chase Marcum <chase.m.marcum@gmail.com>
2014-04-23 00:14:40 -0700

    Adding JQuery Steps to project

--- AIM.Application/AIM.Web.Application/App_Start/BundleConfig.cs
+++ AIM.Application/AIM.Web.Application/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace AIM.Web.Application
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquerysteps").Include(
+                        "~/Scripts/jquery.steps*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
