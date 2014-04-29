7408d9556b4fe3b00fdb7c3dde2c26ffc29a5ab5
johnprice2013 <john.price@oit.edu>
2014-04-28 20:33:47 -0700

    Updated Data Tests to include Asserts

--- AIM/AIM/AIM.Service.Entities/Models/Education.cs
+++ AIM/AIM/AIM.Service.Entities/Models/Education.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using TrackableEntities;
+using System.ComponentModel.DataAnnotations;
 
 namespace AIM.Service.Entities.Models
 {
@@ -11,28 +12,40 @@ namespace AIM.Service.Entities.Models
     public partial class Education : ITrackable
     {
         [DataMember]
+        [Display(Name = "Education ID")]
         public int educationId { get; set; }
         [DataMember]
+        [Display(Name = "School Name")]
         public string schoolName { get; set; }
         [DataMember]
+        [Display(Name = "Degree")]
         public string degree { get; set; }
         [DataMember]
+        [Display(Name = "Graduated")]
         public Nullable<System.DateTime> graduated { get; set; }
         [DataMember]
+        [Display(Name = "Years Attended")]
         public string yearsAttended { get; set; }
         [DataMember]
+        [Display(Name = "Street")]
         public string street { get; set; }
         [DataMember]
+        [Display(Name = "Street 2")]
         public string street2 { get; set; }
         [DataMember]
+        [Display(Name = "City")]
         public string city { get; set; }
         [DataMember]
+        [Display(Name = "State")]
         public Nullable<int> state { get; set; }
         [DataMember]
+        [Display(Name = "Zip Code")]
         public string zip { get; set; }
         [DataMember]
+        [Display(Name = "Applicant ID")]
         public Nullable<int> applicantId { get; set; }
         [DataMember]
+        [Display(Name = "Applicant")]
         public Applicant Applicant { get; set; }
 
         [DataMember]
