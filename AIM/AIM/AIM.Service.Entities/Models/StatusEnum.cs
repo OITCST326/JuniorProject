a8e4e4a6a7980c117d3b1497991f7ef22e4641c8
Chase M. Marcum <chase.m.marcum@gmail.com>
2014-05-08 21:55:17 -0700

    Updated Enum with DataContract and EnumMember

--- AIM/AIM/AIM.Service.Entities/Models/StatusEnum.cs
+++ AIM/AIM/AIM.Service.Entities/Models/StatusEnum.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AIM.Service.Entities.Models
 {
+    [DataContract(Name = "Status")]
     public enum StatusEnum : int
     {
+        [EnumMember]
         InitalApplicationNonSubmitted = 0,
+        [EnumMember]
         PhoneInterviewQueue = 1,
+        [EnumMember]
         InPersonInterviewQueue = 2,
+        [EnumMember]
         CheckReferenceQueue = 3,
+        [EnumMember]
         Rejected = 4,
+        [EnumMember]
         PendingReviewQueue = 5,
+        [EnumMember]
         HiredStatus = 6
     }
 }
--- AIM/AIM/AIM.Service.Entities/Models/TypeEnum.cs
+++ AIM/AIM/AIM.Service.Entities/Models/TypeEnum.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AIM.Service.Entities.Models
 {
+    [DataContract(Name = "QuestionType")]
     public enum TypeEnum : int
     {
+        [EnumMember]
         MultipleChoice = 0,
+        [EnumMember]
         AllThatApply = 1,
+        [EnumMember]
         FreeForm = 2
     }
 }
